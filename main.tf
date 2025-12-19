9ae98ac396621fb32bad4854bc4ecf3f6394f237
siva2j22 <siva2j22@gmail.com>
2025-12-19 16:54:03 +0530

    aws vpc creation

---
+++ main.tf
@@ -0,0 +1,28 @@
+module "vpc" {
+    source = "../terraform-aws-vpc"
+    # vpc_cidr = "10.0.0.1/16"
+    # project_name = "roboshop"
+    # environment = "dev"
+    
+    # vpc
+    vpc_cidr = var.vpc_cidr
+    project_name = var.project_name
+    environment = var.environment
+    vpc_tags = var.vpc_tags
+
+    # public subnets
+    public_subnets_cidrs = var.public_subnets_cidrs
+
+    # private subnets
+    private_subnets_cidrs = var.private_subnets_cidrs
+
+    # private subnets
+    database_subnets_cidrs = var.database_subnets_cidrs
+}
+
+
+
+#  data "aws_availability_zones" "available" {
+#      state = "available"
+# }
+
